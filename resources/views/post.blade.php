9e59c5c8c14a221582732f1106b17a496e17e700
kiashko777 <kiashko777@gmail.com>
2021-10-07 17:48:47 +0400

    create route and blade for post

---
+++ resources/views/post.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/app.css">
+  <title>My Blog</title>
+</head>
+<body>
+<article>
+  <h1><a href="/post">My first post</a></h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos eveniet excepturi officiis provident sunt? Aliquid
+    aperiam asperiores aut, commodi consectetur consequuntur cupiditate delectus dicta eaque est et eveniet ipsa ipsum
+    iste iure iusto molestias odio placeat praesentium quaerat quibusdam recusandae, reiciendis reprehenderit sequi sit
+    tempore, temporibus totam ut voluptate voluptates!</p>
+</article>
+<a href="/">Go Back</a>
+
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/app.css">
+  <title>My Blog</title>
+</head>
+<body>
+<article>
+  <h1><a href="/post">My first post</a></h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A debitis exercitationem in laboriosam optio perferendis
+    porro sit, voluptatem? Accusantium adipisci aliquam distinctio doloribus dolorum eligendi, error ex incidunt iusto
+    modi, necessitatibus non quidem quod repellendus sed sunt, suscipit unde vitae.</p>
+</article>
+
+<article>
+  <h1><a href="/post">My second post</a></h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A debitis exercitationem in laboriosam optio perferendis
+    porro sit, voluptatem? Accusantium adipisci aliquam distinctio doloribus dolorum eligendi, error ex incidunt iusto
+    modi, necessitatibus non quidem quod repellendus sed sunt, suscipit unde vitae.</p>
+</article>
+
+<article>
+  <h1><a href="/post">My third post</a></h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A debitis exercitationem in laboriosam optio perferendis
+    porro sit, voluptatem? Accusantium adipisci aliquam distinctio doloribus dolorum eligendi, error ex incidunt iusto
+    modi, necessitatibus non quidem quod repellendus sed sunt, suscipit unde vitae.</p>
+</article>
+
+
+</body>
+</html>
