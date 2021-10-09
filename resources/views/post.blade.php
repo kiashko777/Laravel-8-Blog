31d92508082ceeccf4515515e85f8ea81bed1b8d
kiashko777 <kiashko777@gmail.com>
2021-10-09 17:12:51 +0400

    implement blade elements

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,8 +10,8 @@
 </head>
 <body>
 <article>
-  <h1><?= $post->title; ?></h1>
-  <div><?= $post->body; ?></div>
+  <h1>{{ $post->title }}</h1>
+  <div>{!!$post->body!!}</div>
 </article>
 <a href="/">Go Back</a>
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,17 +9,17 @@
   <title>My Blog</title>
 </head>
 <body>
-<?php foreach ($posts as $post): ?>
-<article>
-  <h1>
-    <a href="/posts/<?=$post->slug; ?>">
-      <?=$post->title; ?>
-    </a>
-  </h1>
-  <div><?= $post->excerpt; ?></div>
-</article>
+@foreach($posts as $post)
+  <article>
+    <h1>
+      <a href="/posts/{{$post->slug}}">
+        {{$post->title}}
+      </a>
+    </h1>
+    <div>{{$post->excerpt}}</div>
+  </article>
 
-<?php endforeach; ?>
+@endforeach
 
 
 </body>
