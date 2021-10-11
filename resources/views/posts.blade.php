7114e7b1b2b0eb2a67963527c143349d8204693b
kiashko777 <kiashko777@gmail.com>
2021-10-11 15:22:49 +0400

    create DB,run migrations and create model

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,11 +4,11 @@
   @foreach($posts as $post)
     <article>
       <h1>
-        <a href="/posts/{{$post->slug}}">
-          {{$post->title}}
+        <a href="/posts/{{ $post->id }}">
+          {{ $post->title }}
         </a>
       </h1>
-      <div>{{$post->excerpt}}</div>
+      <div>{{ $post->excerpt }}</div>
     </article>
 
   @endforeach
