4660122e58ab4a3ce8ead1cc9121f7a9374cd407
kiashko777 <kiashko777@gmail.com>
2021-10-12 11:34:58 +0400

    fix relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,8 @@
 @section('content')
   <article>
     <h1>{{ $post->title }}</h1>
+    <a href="#">Category: {{ $post->category->name }}</a>
+    <br><br>
     <div>{!!$post->body!!}</div>
   </article>
   <br>
