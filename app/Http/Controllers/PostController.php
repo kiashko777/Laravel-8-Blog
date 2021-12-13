d90b01a87837d1870dcef175e13c2e09eab547c2
kiashko777 <kiashko777@gmail.com>
2021-12-13 15:10:46 +0400

    fix:move logic from routes to controller
    all logic from routes moved to controller methods

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 
 class PostController extends Controller
 {
@@ -25,6 +26,29 @@ class PostController extends Controller
   {
     return view('posts.show', [
       'post' => $post,
+    ]);
+  }
+
+
+  //  METHOD TO SEE ALL AUTHOR'S POSTS
+
+  public function getAuthor(User $author)
+  {
+    return view('posts.index', [
+      'posts' => $author->posts->load(['category', 'author']),
+      'categories' => Category::all(),
+    ]);
+  }
+
+
+  //  METHOD TO SEE ALL POSTS OF CATEGORY SELECTED
+
+  public function getCategory(Category $category)
+  {
+    return view('posts.index', [
+      'posts' => $category->posts->load(['category', 'author']),
+      'carrentCategory' => $category,
+      'categories' => Category::all(),
     ]);
   }
 }
