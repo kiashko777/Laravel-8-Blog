004806c4221f22594d03a697f041a0aee0c032de
kiashko777 <kiashko777@gmail.com>
2021-12-30 17:23:31 +0400

    fix:named routes implement
    named routes implemented instead hardcoded ones

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -9,75 +9,75 @@ use App\Models\Post;
 
 class AdminPostController extends Controller
 {
-    /**
-     *  Method to see all posts from admin section.
-     * */
-    public function index()
-    {
-        return view('admin.posts.index', [
+  /**
+   *  Method to see all posts from admin section.
+   * */
+  public function index()
+  {
+    return view('admin.posts.index', [
       'posts' => Post::paginate(10),
     ]);
-    }
+  }
 
-    /**
-     * Method to create post.
-     * */
-    public function create(Category $category)
-    {
-        return view('admin.posts.create', [
+  /**
+   * Method to create post.
+   * */
+  public function create(Category $category)
+  {
+    return view('admin.posts.create', [
       'categories' => Category::all(),
     ]);
-    }
+  }
 
-    /**
-     *  Method to validate and store the post.
-     * */
-    public function store(StorePostRequest $request)
-    {
-        $attributes = $request->validated();
+  /**
+   *  Method to validate and store the post.
+   * */
+  public function store(StorePostRequest $request)
+  {
+    $attributes = $request->validated();
 
-        $attributes['user_id'] = auth()->id();
-        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
+    $attributes['user_id'] = auth()->id();
+    $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
 
-        Post::create($attributes);
+    Post::create($attributes);
 
-        return redirect(route('home'));
-    }
+    return redirect(route('home'));
+  }
 
-    /**
-     *   Method to edit the post.
-     * */
-    public function edit(Post $post, Category $category)
-    {
-        return view('admin.posts.edit', [
+  /**
+   *   Method to edit the post.
+   * */
+  public function edit(Post $post, Category $category)
+  {
+    return view('admin.posts.edit', [
       'post' => $post,
       'categories' => Category::all(),
     ]);
+  }
+
+  /**
+   * Method to update the post.
+   * */
+  public function update(UpdatePostRequest $request, Post $post)
+  {
+    $attributes = $request->validated();
+
+    if (isset($attributes['thumbnail'])) {
+      $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
     }
 
-    /**
-     * Method to update the post.
-     * */
-    public function update(UpdatePostRequest $request, Post $post)
-    {
-        $attributes = $request->validated();
+    $post->update($attributes);
 
-        if (isset($attributes['thumbnail'])) {
-            $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
-        }
+    return back()->with('success', 'Post Updated!');
+  }
 
-        $post->update($attributes);
+  /**
+   * Method to delete the post.
+   * */
+  public function destroy(Post $post)
+  {
+    $post->delete();
 
-        return back()->with('success', 'Post Updated!');
-    }
-
-    /**
-     * Method to delete the post.
-     * */
-    public function destroy(Post $post)
-    {
-        $post->delete();
-
-        return back()->with('success', 'Post Deleted!');
-    }
+    return back()->with('success', 'Post Deleted!');
+  }
 }
